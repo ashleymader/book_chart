d7bae9b321bd537ccefa0475f19562afd601de8c
ashleymader <ashley.mader12@gmail.com>
2020-03-26 08:43:09 -0400

    added scraper links

--- lib/book_chart/scraper.rb
+++ lib/book_chart/scraper.rb
@@ -1,10 +1,30 @@
 class Scraper 
 
-    def get_all_charts
+    def get_most_sold_f
         site = "https://www.amazon.com/charts/2020-03-22/mostread/fiction?ref=chrt_bk_dx_intra_fc_rd"
         doc = Nokogiri::HTML(open(site))
+        @most_sold_f = doc.css("div.kc-header .kc-navigation a").attr("href")
+       
     end
     
+    def get_most_sold_nf 
+        site = "https://www.amazon.com/charts/2020-03-22/mostsold/fiction?ref=chrt_bk_dx_intra_sd_fc"
+        doc = Nokogiri::HTML(open(site))
+        @most_sold_nf = doc.css("div.sntra-page-content .kc-sublist a").attr("href") 
+    end
+
+    def get_most_read_f
+        site = "https://www.amazon.com/charts/2020-03-22/mostread/fiction?ref=chrt_bk_dx_intra_fc_rd"
+        doc = Nokogiri::HTML(open(site))
+        @most_read_f = doc.css("div.sntra-page-content a").attr("href")
+    end
+
+    def get_most_read_nf
+        site = "https://www.amazon.com/charts/2020-03-22/mostread/fiction?ref=chrt_bk_dx_intra_fc_rd"
+        doc = Nokogiri::HTML(open(site))
+        @most_read_nf = doc.css("div.sntra-page-content .kc-sublist a").attr("href")
+    end
+
     def list_charts
 
     end
